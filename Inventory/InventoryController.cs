4b0b8fdc5e6c2f2fa9f82c23f70b41592eba5643
Theodorus Doni <theodorusdoni4@gmail.com>
2021-03-26 08:12:06 +0700

    Migrate project to github repo

---
+++ Inventory/InventoryController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lncodes.Example.Delegate
+{
+    public sealed class InventoryController
+    {
+        public readonly int MaxCapacity = 3;
+        private readonly List<string> _itemCollection = new List<string>();
+
+        //Declare Delegate Using Delegate Keyword
+        public delegate void DeleteItemCallback(int itemIndex, string itemName);
+        private readonly DeleteItemCallback _deleteItemCallback;
+
+        //Constructor
+        public InventoryController(DeleteItemCallback deleteItem) =>
+            _deleteItemCallback = deleteItem;
+
+        /// <summary>
+        /// Method for adding item to inventory
+        /// </summary>
+        /// <param name="item">Item Want To Add To Inventory</param>
+        /// <param name="addItemCallback">Delegate Call When Success Adding Item</param>
+        public void AddingItem(string item, Action<string> addItemCallback)
+        {
+            if (_itemCollection.Count < MaxCapacity)
+            {
+                _itemCollection.Add(item);
+                addItemCallback(item);
+            }
+            else Console.WriteLine("Your inventory has reached max capacity");
+        }
+
+        /// <summary>
+        /// Method for adding item to inventory
+        /// </summary>
+        /// <param name="getRandomItem">Delegate Return Item</param>
+        /// <param name="addItemCallback">Delegate Call When Success Adding Item</param>
+        public void AddingItem(Func<string> getRandomItem, Action<string> addItemCallback)
+        {
+            if (_itemCollection.Count < MaxCapacity)
+            {
+                var item = getRandomItem();
+                _itemCollection.Add(item);
+                addItemCallback(item);
+            }
+            else Console.WriteLine("Your inventory has reached max capacity");
+        }
+
+        /// <summary>
+        /// Method for adding item to inventory
+        /// </summary>
+        /// <param name="getRandomItem">Delegate Return Item</param>
+        /// <param name="addItemCallback">Delegate Call When Success Adding Item</param>
+        /// <param name="canAddingItems">Delegate Check If Can Adding New Item</param>
+        public void AddingItem(Func<string> getRandomItem, Action<string> addItemCallback, Predicate<int> canAddingItems)
+        {
+            if (canAddingItems(_itemCollection.Count))
+            {
+                var item = getRandomItem();
+                _itemCollection.Add(item);
+                addItemCallback(item);
+            }
+            else Console.WriteLine("Your inventory has reached max capacity");
+        }
+
+        /// <summary>
+        /// Method for delete item from inventory
+        /// </summary>
+        /// <param name="itemIndex">Item index that want to delete</param>
+        public void DeleteItem(int itemIndex)
+        {
+            var itemName = _itemCollection[itemIndex];
+            _itemCollection.RemoveAt(itemIndex);
+            _deleteItemCallback(itemIndex, itemName);
+        }
+
+        /// <summary>
+        /// Method to show all item in inventory
+        /// </summary>
+        public void ShowAllItem()
+        {
+            Console.WriteLine();
+            Console.WriteLine("All items in inventory");
+            _itemCollection.ForEach(Console.WriteLine);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace Lncodes.Example.Delegate
+{
+    public class Program
+    {
+        static void Main()
+        {
+            //Create delete item delegate
+            var deleteItemCallback = new InventoryController.DeleteItemCallback(
+                (itemIndex, itemName) => Console.WriteLine($"Sucess delete items {itemName} at index {itemIndex}")
+            );
+
+            //Create InventoryController instance
+            var inventoryController = new InventoryController(deleteItemCallback);
+            AddItemToInventory(inventoryController);
+            DeleteItemFromInveotry(inventoryController);
+            ShowAllItem(inventoryController);
+        }
+
+        /// <summary>
+        /// Method to adding item to inventory
+        /// </summary>
+        /// <param name="inventoryController"></param>
+        private static void AddItemToInventory(InventoryController inventoryController)
+        {
+            //Calling Inventory Controller Method
+            inventoryController.AddingItem("Potion",
+                (item) => Console.WriteLine($"Sucess Adding Item : {item}"));
+
+            inventoryController.AddingItem(GetRandomItem,
+                (item) => Console.WriteLine($"Sucess Adding Item : {item}"));
+
+            inventoryController.AddingItem(GetRandomItem,
+                (item) => Console.WriteLine($"Sucess Adding Item : {item}"),
+                (ammountOfItem) => ammountOfItem < inventoryController.MaxCapacity);
+        }
+
+        /// <summary>
+        /// Method for delete item in inventory
+        /// </summary>
+        /// <param name="inventoryController"></param>
+        private static void DeleteItemFromInveotry(InventoryController inventoryController)
+        {
+            int deletedItemIndex = 1;
+            inventoryController.DeleteItem(deletedItemIndex);
+        }
+
+        /// <summary>
+        /// Method to show all item in inventory
+        /// </summary>
+        /// <param name="inventoryController"></param>
+        private static void ShowAllItem(InventoryController inventoryController) =>
+            inventoryController.ShowAllItem();
+
+        /// <summary>
+        /// Method for random item
+        /// </summary>
+        /// <returns>A String Represent Random Item For Add To Inventory</returns>
+        /// <exception cref="Exception">Thrown when random value > 3</exception>
+        private static string GetRandomItem()
+        {
+            switch (new Random().Next(3))
+            {
+                case 0:
+                    return "Weapon";
+                case 1:
+                    return "Potion";
+                case 2:
+                    return "Armor";
+                default:
+                    throw new Exception("Error random item");
+            }
+        }
+    }
+}
